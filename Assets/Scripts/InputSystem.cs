694aa05be0e17b3d9055ac33974e09a7aaa4c87c
zapto123 <tsoorckanukris@gmail.com>
2021-01-02 17:55:46 +0200

    cleared some stuff

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -5,15 +5,10 @@ using System;
 using System.IO;
 using UnityEngine;
 
-#region InputSystem
-
 public class InputSystem : MonoBehaviour
 {
-    Vector2 rawInputVector = Vector2.zero;
-    public Queue<InputFrame<Vector2>> movementInputQueue = new Queue<InputFrame<Vector2>>();
+    // public Queue<InputFrame<Vector2>> movementInputQueue = new Queue<InputFrame<Vector2>>();
     
-    // Start is called before the first frame update
-
     private Stopwatch stopwatch = new Stopwatch();
     private TimeSpan previousTime;
 
@@ -22,23 +17,14 @@ public class InputSystem : MonoBehaviour
         stopwatch.Start();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // print(rawInputVector);
-        rawInputVector = Vector2.zero;
         
     }
 
     void OnGUI() {
-
-        rawInputVector += (Input.GetAxisRaw("Horizontal") * Vector2.right
-            + Input.GetAxisRaw("Vertical") * Vector2.up)
-            * (stopwatch.Elapsed - previousTime).Milliseconds / 1000;   //ms to s
-        
-        print((stopwatch.Elapsed - previousTime));
-
-        previousTime = stopwatch.Elapsed;
+        if (Event.current.ToString() != "Layout" && Event.current.ToString() != "repaint")
+            print(Event.current);
     }
 }
 
@@ -46,56 +32,3 @@ public class InputFrame<T> {
     public T Value {get; set; }
     public float Timestep { get; set; }
 }
-
-#endregion
-
-#region KeybindConfig
-//keep this class persistent between scenes
-public class KeybindManager : MonoBehaviour {
-
-    private KeybindConfig currentConfig;
-    private const string configFilePath = Application.dataPath + "inputConfig.cfg";
-
-    void Start() {
-        ReloadConfig();
-    }
-
-    private void ReloadConfig()
-    {
-        try 
-        {
-            var config = JsonUtility.FromJson<KeybindConfig>(File.ReadAllText(configFilePath));
-        } 
-        catch (Exception e) 
-        {
-            print(e);
-            ResetKeybindsConfig();
-        }
-    }
-
-    public void ResetKeybindsConfig() {
-        //create default config object
-        SaveConfig();
-    }
-
-    public void SetKeybind(KeybindConfig newConfig) {
-        currentConfig = newConfig;
-        SaveConfig();
-    }
-
-    public void SaveConfig() {
-        //save current config to file
-    }
-}
-
-[Serializable]
-public class KeybindConfig {
-    public KeyCode KeybindUp { get; set; } = KeyCode.W;
-    public KeyCode KeybindDown { get; set; } = KeyCode.S;
-    public KeyCode KeybindLeft { get; set; } = KeyCode.A;
-    public KeyCode KeybindRight { get; set; } = KeyCode.D;
-    public KeyCode KeybindEnter { get; set; } = KeyCode.Return;
-    public KeyCode KeybindX { get; set; } = KeyCode.X;
-}
-
-#endregion
---
+++ Assets/Scripts/KeybindConfig.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System;
+using UnityEngine;
+
+//keep this class persistent between scenes
+public class KeybindManager : MonoBehaviour {
+    private KeybindConfig currentConfig;
+    private string configFilePath;
+
+    void Start() {
+        configFilePath = Application.dataPath + "inputConfig.cfg";
+        ReloadConfig();
+    }
+
+    private void ReloadConfig()
+    {
+        try 
+        {
+            var config = JsonUtility.FromJson<KeybindConfig>(File.ReadAllText(configFilePath));
+        } 
+        catch (Exception e) 
+        {
+            print(e);
+            ResetKeybindsConfig();
+        }
+    }
+
+    public void ResetKeybindsConfig() {
+        //create default config object
+        SaveConfig();
+    }
+
+    public void SetKeybind(KeybindConfig newConfig) {
+        currentConfig = newConfig;
+        SaveConfig();
+    }
+
+    public void SaveConfig() {
+        //save current config to file
+    }
+}
+
+public enum GameAction {
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    Hook
+}
+
+public class Keybind {
+    public GameAction action;
+    public KeyCode keyCode;
+
+    public Keybind(GameAction action, KeyCode keyCode) {
+        this.action = action;
+        this.keyCode = keyCode;
+    }
+}
+
+[Serializable]
+public class KeybindConfig {
+    public Keybind[] keybinds = new Keybind[] {
+        new Keybind(GameAction.MoveUp, KeyCode.W),
+        new Keybind(GameAction.MoveDown, KeyCode.S),
+        new Keybind(GameAction.MoveLeft, KeyCode.A),
+        new Keybind(GameAction.MoveRight, KeyCode.D),
+        new Keybind(GameAction.Hook, KeyCode.Space),
+    };
+}
