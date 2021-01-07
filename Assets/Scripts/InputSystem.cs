6653c20719a3fec0637ffeba3a3a8f3c23138941
PunkyIANG <tsoorckanukris@gmail.com>
2021-01-07 12:15:33 +0200

    fixed input system
    input system now doesn't require queue to work properly

--- Assets/Scripts/InputSystem.cs
+++ Assets/Scripts/InputSystem.cs
@@ -11,7 +11,8 @@ public class InputSystem : MonoBehaviour
 {
     public KeybindManager keybindManager;
     private Stopwatch stopwatch = new Stopwatch();
-    private Dictionary<GameAction, QueueFP<InputChange>> inputQueue;
+    private Dictionary<GameAction, Queue<InputChange>> inputQueue;
+    private Dictionary<GameAction, EventType> lastEvent;
 
     public delegate void InputEventHandler(GameAction action, InputChange inputChange);
 
@@ -22,16 +23,20 @@ public class InputSystem : MonoBehaviour
     void Init(KeybindManager keybindManager) {
         this.keybindManager = keybindManager;
     }
+    
     void Start()
     {
-        inputQueue = new Dictionary<GameAction, QueueFP<InputChange>>();
+        inputQueue = new Dictionary<GameAction, Queue<InputChange>>();
+        lastEvent = new Dictionary<GameAction, EventType>();
 
         //for each game action
         //add an entry to inputQueue
         foreach (GameAction action in Enum.GetValues(typeof(GameAction)))
         {
-            inputQueue.Add(action, new QueueFP<InputChange>());
-            inputQueue[action] = new QueueFP<InputChange>();
+            inputQueue.Add(action, new Queue<InputChange>());
+            inputQueue[action] = new Queue<InputChange>();
+
+            lastEvent[action] = EventType.KeyUp;
         }
 
         stopwatch.Start();
@@ -48,22 +53,20 @@ public class InputSystem : MonoBehaviour
                 .Where(k => k.keyCode == currKeycode)
                 .Select(k => k.action))
             {
-                if (inputQueue[action].FrontPeek() == null ||
-                    inputQueue[action].FrontPeek().Value != currEventType)
+                if (lastEvent[action] != currEventType)
                 {
                     var inputChange = new InputChange
                     {
                         Value = currEventType,
                         Timestep = stopwatch.Elapsed
                     };
+
+                    lastEvent[action] = currEventType;
                     
                     inputQueue[action].Enqueue(inputChange);
 
                     InputEvent(action, inputChange);
                     
-                    //still kinda shitty that the system relies on the queue's last element for comparison
-                    //TODO: set the last input event as a separate variable 
-
                     //debug
                     // var lastEvent = inputQueue[action].FrontPeek();
                     // print(action + " " + lastEvent.Value + " " + lastEvent.Timestep);
@@ -77,20 +80,4 @@ public class InputChange
 {
     public EventType Value { get; set; }
     public TimeSpan Timestep { get; set; }
-}
-
-public class QueueFP<T> : Queue<T>
-{
-    private T latestVar = default(T); //kinda null but also supports non-null // also kinda redundant
-
-    public T FrontPeek()
-    {
-        return latestVar;
-    }
-
-    public new void Enqueue(T item)
-    {
-        latestVar = item;
-        base.Enqueue(item);
-    }
 }
